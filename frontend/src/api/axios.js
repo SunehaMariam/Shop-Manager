02c4d899700ba7fd7a2cd356fff04c7037ab6a5b
Suneha Mariam <rehanagill260@gmail.com>
2026-09-17 17:03:37 +0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api'
+  baseURL: 'https://shop-manager-six-lemon.vercel.app/api'
 });
 
 // Har request ke sath token
